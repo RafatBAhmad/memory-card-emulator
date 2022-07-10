b33f92871958e9cc05b05d6b3abef4612ee6e5db
‪Rafat Ahmad‬‏ <37984571+RafatBAhmad@users.noreply.github.com>
2022-07-10 13:06:36 +0300

    add storage class named memoryCmd

--- app.cpp
+++ app.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-
+#include <list>
 using namespace std;
+
+class memoryCmd
+{
+    list<int> memory;
+
+public:
+    int Read(int LBA)
+    {
+        auto result = memory.begin();
+
+        std::advance(result, LBA);
+
+        return *result;
+    }
+
+    void Write(int LBA, int value)
+    {
+        auto result = memory.begin();
+
+        std::advance(result, LBA);
+
+        *result = value;
+    }
+
+    void Delete(int startIndex, int endIndex)
+    {
+        auto start = memory.begin();
+        auto end = memory.begin();
+        std::advance(start, startIndex);
+        std::advance(end, endIndex + 1);
+        memory.erase(start, end);
+    }
+};
 
 int main()
 {
